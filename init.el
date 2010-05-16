7ae0e080437c650f6e9be52094cbc00e32cb7727
John Evans <john@milo.com>
2010-05-15 21:01:14 -0700

    Added desktop saving.

--- init.el
+++ init.el
@@ -32,17 +32,18 @@
 (setq-default indent-tabs-mode nil)
 
 
-;; Enable upcase/downcasing a region with C-x C-u and C-x C-l respectively.
+;; Enable upcase/downcasing a region with C-x C-u and C-x C-l
+;; respectively.
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 
 
-;; The emacs-textmate package makes emacs mimic Textmate's behavior with
-;; regards to matching pairs of e.g. quotes, parens, etc.
-;; For example, if you type a quote it will add the closing quote
+;; The emacs-textmate package makes emacs mimic Textmate's behavior
+;; with regards to matching pairs of e.g. quotes, parens, etc.  For
+;; example, if you type a quote it will add the closing quote
 ;; automatically and then position the cursor inside the two quotes...
-;; but then if you hit another quote it will replace the second one (even if
-;; you already typed something)
+;; but then if you hit another quote it will replace the second one
+;; (even if you already typed something)
 
 (load-file "~/.emacs.d/lib/emacs-textmate.el")
 (textmate-mode)
@@ -86,6 +87,32 @@
 ;; We want to know when we're leaving behind trailing whitespace.
 (setq show-trailing-whitespace t)
 (setq-default show-trailing-whitespace t)
+
+
+;; Save a list of open files in ~/.emacs.desktop
+;; Execute "M-x desktop-save" once and it will update whenever auto-save
+;; occurs (and on every exit?)
+(require 'desktop)
+(desktop-save-mode 1)
+(add-hook 'auto-save-hook
+          (lambda () (desktop-save-in-desktop-dir)))
+
+;; Save a bunch of variables to the desktop file for lists specify the
+;; len of the maximal saved data also
+(setq desktop-globals-to-save
+      (append '((extended-command-history . 30)
+                (file-name-history        . 100)
+                (grep-history             . 30)
+                (compile-history          . 30)
+                (minibuffer-history       . 50)
+                (query-replace-history    . 60)
+                (read-expression-history  . 60)
+                (regexp-history           . 60)
+                (regexp-search-ring       . 20)
+                (search-ring              . 20)
+                (shell-command-history    . 50)
+                tags-file-name
+                register-alist)))
 
 
 ;;; init.el -- The End.
