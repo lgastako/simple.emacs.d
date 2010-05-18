f7ad5d0719c8db61624c67ff80bb8b083de5b572
John Evans <john@milo.com>
2010-05-17 17:22:24 -0700

    Added nyx.milo configuation.

---
+++ nyx.milo.com.el
@@ -0,0 +1,25 @@
+(setenv "PYTHONPATH" "/pluto/pluto:/pluto/pycloud")
+(setenv "LD_LIBRARY_PATH" "/pluto/local/lib:/usr/local/lib")
+(setenv "PATH" "/pluto/local/bin:/pluto/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games")
+
+(when (load "flymake" t)
+  (defun flymake-pylint-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "/pluto/pycloud/apps/emacs/bin/lintrunner.py" (list local-file))))
+
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pylint-init)))
+
+(add-hook 'python-mode-hook (lambda() (flymake-mode t)))
+
+;; (load "~/.emacs.d/vendor/pymacs.el")
+;; (require 'pymacs)
+;; (autoload 'pymacs-apply "pymacs")
+;; (autoload 'pymacs-call "pymacs")
+;; (autoload 'pymacs-eval "pymacs" nil t)
+;; (autoload 'pymacs-exec "pymacs" nil t)
+;; (autoload 'pymacs-load "pymacs" nil t)
